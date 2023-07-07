6e0017fbd116677e026ec3aa53701448513f5546
Himanshu Sharma <79913533+hsuecu@users.noreply.github.com>
2023-07-07 21:53:11 +0530

    Add files via upload

---
+++ include/onair.h
@@ -0,0 +1,174 @@
+
+#include <iostream>
+#include <fstream>
+
+#include <complex>
+#include <string>
+#include <map>
+#include <cmath>
+#include <vector>
+
+#include <inttypes.h>
+#include <unistd.h>
+#include <ctime>
+#include <alsa/asoundlib.h>
+
+/*alsa specific*/
+#define ALSA_PCM_NEW_HW_PARAMS_API
+
+#define HIGH 1
+#define LOW 0
+#define PI_2 6.283185307179586
+
+/*how many times the flag will be send before and after
+  data transmission
+*/
+#define FLG_REPEAT_COUNT 3
+
+using namespace std;
+
+namespace Uplink
+{
+    enum FlagType
+    {
+        sender_sync_flg,
+        sender_fin_flg
+    };
+
+    class DifferentialEncoderOscillator
+    {
+        public:
+        float q1, q0, sample_rate, angle, factor_high, factor_low;
+        DifferentialEncoderOscillator(void);
+        DifferentialEncoderOscillator(float _q1, float _q0, float sr);
+        float sample(int state);
+    };
+
+    class HexPulseConvertor {
+        vector<vector<int>> encoding_map = {
+            {1,0,0},               // 0 .
+            {1,1,0,0},             // 1 -
+            {1,0,1,0,0},           // 2 ..
+            {1,0,1,1,0,0},         // 3 .-
+            {1,1,0,1,0,0},         // 4 -.
+            {1,1,0,1,1,0,0},       // 5 --
+            {1,0,1,0,1,0,0},       // 6 ...
+            {1,0,1,0,1,1,0,0},     // 7 ..-
+            {1,0,1,1,0,1,0,0},     // 8 .-.
+            {1,0,1,1,0,1,1,0,0},   // 9 .--
+            {1,1,0,1,0,1,0,0},     // A -..
+            {1,1,0,1,0,1,1,0,0},   // B -.-
+            {1,1,0,1,1,0,1,0,0},   // C --.
+            {1,1,0,1,1,0,1,1,0,0}, // D ---
+            {1,0,1,0,1,0,1,0,0},   // E ....
+            {1,0,1,0,1,0,1,1,0,0}, // F ...-
+            {0,0,0,0,0,0,0,0,0,0}, // SYNC
+            {1,1,1,1,1,1,1,1,1,1}  // FIN
+        };
+    public:
+        vector<int> convert(char data);
+        vector<int> sync();
+        vector<int> finish();
+    };
+
+    class Sender {
+        /*sender config variable*/
+        int32_t sample_width, max_amp = 2147483647, bit_depth = 32;
+        unsigned int sample_rate, period;
+        DifferentialEncoderOscillator oscillator;
+        HexPulseConvertor convertor;
+
+        /*alsa control variables*/
+        int pcm, dir;
+        snd_pcm_t* pcm_handle;
+        snd_pcm_hw_params_t* params;
+        snd_pcm_uframes_t frames;
+
+        /*buffer configuration*/
+        int32_t* buffer;
+        int32_t buffer_size = 0, buffer_counter = 0;
+
+        /*sleep estimator*/
+        unsigned long frame_cnt = 0;
+        unsigned int start_time;
+        unsigned int end_time;
+
+    public:
+        Sender();
+        void flush();
+        void relayFlag(FlagType type);
+        void sleep_estimator();
+        void sync(int msg_len);
+        void dsync();
+        void put(char c);
+        void puts(string str);
+        ~Sender();
+    };
+
+};
+
+
+
+namespace Downlink{
+    enum ProcState{ 
+        init,
+        gather_sync_flg_bits
+    };
+
+    class Decoder{
+        int32_t count = 0, flip = 0, expected_len = 0;
+        const int32_t sync_flg_len = 250;
+        const int32_t sync_flg_bitlen = 10;
+        const int32_t sync_flg_pop_times = 3;
+        bool sync_state = false;
+        ProcState procState = init;
+        string buffer = "";
+        char ret_word = 0x00;
+        int flip_flop = 0;
+
+        bool working;
+        bool was_word_seen;
+
+        unsigned int char_cnt;
+
+        map<string, char> translator;
+    public:
+        Decoder();
+        void clean_buffer();
+        bool isWorking();
+        void char_cntReset();
+        bool char_cntGet();
+        void trySync(int value);
+        int process(int value);
+    };
+
+    class Receiver {
+        // dft control variables
+        float q_1, q_0, sample_rate;
+        int32_t dft_window = 1920;
+        int32_t dft_repeat_factor = 96;
+        vector<complex<float>> dft_factor_high;
+        vector<complex<float>> dft_factor_low;
+        vector<float> reading_high;
+        vector<float> reading_low;
+
+        // alsa variables
+        int pcm, dir;
+        snd_pcm_t* pcm_handle;
+        snd_pcm_hw_params_t* params;
+        snd_pcm_uframes_t frames;
+
+        // buffer control variables
+        int32_t* buffer;
+        int32_t buffer_size;
+        Decoder decoder;
+
+    public:
+        Receiver(void);
+        Receiver(float q1, float q0, float sr);
+        void listen();
+        void start();
+        bool isBreak();
+        ~Receiver();
+    };
+};
---
+++ src/onair.c
@@ -0,0 +1,386 @@
+#include "../include/onair.h"
+
+Uplink::DifferentialEncoderOscillator::DifferentialEncoderOscillator(void){
+
+}
+
+Uplink::DifferentialEncoderOscillator::DifferentialEncoderOscillator(float _q1, float _q0, float sr){
+    q1 = _q1; q0 = _q0;
+    sample_rate = sr;
+    factor_high = PI_2 * q1 / sample_rate;
+    factor_low = PI_2 * q0 / sample_rate;
+}
+
+float Uplink::DifferentialEncoderOscillator::sample(int state) {
+    if(angle > PI_2) angle = fmod(angle, PI_2);
+    if(state > 0) angle += factor_high;
+    else angle += factor_low;
+    return 0.5 * sin(angle);
+}
+
+vector<int> Uplink::HexPulseConvertor::convert(char data) {
+    vector<int> ret, tmp;
+    ret = encoding_map[(data) & 0x0f];
+    tmp = encoding_map[(data >> 4) & 0x0f];
+    ret.insert(ret.begin(), tmp.begin(), tmp.end());
+    return ret;
+}
+
+vector<int> Uplink::HexPulseConvertor::sync() {
+    return encoding_map[16];
+}
+
+vector<int> Uplink::HexPulseConvertor::finish() {
+    return encoding_map[17];
+}
+
+
+
+Uplink::Sender::Sender(){
+    // upon observation sample_width must be equal to frames size
+    sample_width = 1920;
+    sample_rate = 384000;
+    frames = 1920;
+    oscillator = DifferentialEncoderOscillator(19000, 18800, 384000);
+    convertor = HexPulseConvertor();
+
+    pcm = snd_pcm_open(&(pcm_handle), "default", SND_PCM_STREAM_PLAYBACK, 0);
+    assert(pcm >= 0);
+
+    snd_pcm_hw_params_alloca(&(params));
+
+    pcm = snd_pcm_hw_params_any(pcm_handle, params);
+    assert(pcm >= 0);
+
+    pcm = snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+    assert(pcm >= 0);
+
+    pcm = snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S32);
+    assert(pcm >= 0);
+
+    pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &(sample_rate), &(dir));
+    assert(pcm >= 0);
+
+    pcm = snd_pcm_hw_params_set_period_size_near(pcm_handle, params, &(frames), &(dir));
+    assert(pcm >= 0);
+
+    pcm = snd_pcm_hw_params(pcm_handle, params);
+    assert(pcm >= 0);
+
+    snd_pcm_hw_params_get_period_size(params, &(frames), &(dir));
+
+    buffer_size = frames;
+    buffer = (int32_t*) malloc(sizeof(int32_t)*(buffer_size));
+}
+
+void Uplink::Sender::flush() {
+    pcm = snd_pcm_writei(pcm_handle, buffer, buffer_size);
+    if(pcm < 0) cout << snd_strerror(pcm) << endl;
+    assert(pcm >= 0);
+}
+
+void Uplink::Sender::relayFlag(FlagType type) {
+    vector<int> ret;
+    if(type == sender_sync_flg) {
+        ret = convertor.sync();
+    }
+    else {
+        ret = convertor.finish();
+    }
+    for(int times = 0; times < FLG_REPEAT_COUNT; times++){
+        for(int bit: ret) {
+            for(int n = 0; n < sample_width; n++) {
+                buffer[(buffer_counter)++] = max_amp * oscillator.sample(bit);
+                if(buffer_counter == buffer_size) {
+                    flush();
+                    buffer_counter = 0;
+                }
+            }
+        }
+    }
+}
+
+void Uplink::Sender::sleep_estimator() {
+    unsigned int diff = end_time - start_time;
+    unsigned int ret = frame_cnt / 200;
+    ret = ret - diff;
+    if(ret > 0) {
+        sleep(ret+1);
+    }
+    else {
+        usleep(5000 * frame_cnt + 500000);
+    }
+}
+
+void Uplink::Sender::sync(int msg_len) {
+    //if(msg_len < 12) sleep(1);
+    //else if(msg_len < 24) sleep(2);
+    //else sleep(3); // 3 seconds is just right enough
+
+    frame_cnt = 3;
+    start_time = time(NULL);
+
+    pcm = snd_pcm_prepare(pcm_handle);
+    assert(pcm >= 0);
+    relayFlag(sender_sync_flg);
+}
+
+void Uplink::Sender::dsync() {
+
+    frame_cnt += 3;
+
+    relayFlag(sender_fin_flg);
+    end_time = time(NULL);
+}
+
+void Uplink::Sender::put(char c) {
+    auto ret = convertor.convert(c);
+    for(auto bit: ret) {
+        frame_cnt ++;
+        for(int n = 0; n < sample_width; n++) {
+            buffer[buffer_counter++] = max_amp * oscillator.sample(bit);
+            if(buffer_counter == buffer_size) {
+                flush();
+                buffer_counter = 0;
+            }
+        }
+    }
+}
+
+void Uplink::Sender::puts(string str) {
+    for(auto c: str) {
+        put(c);
+    }
+}
+
+Uplink::Sender::~Sender() {
+    snd_pcm_drain(pcm_handle);
+    snd_pcm_close(pcm_handle);
+    free(buffer);
+}
+
+Downlink::Decoder::Decoder(){
+    translator["."] = 0x00;
+    translator["-"] = 0x01;
+    translator[".."] = 0x02;
+    translator[".-"] = 0x03;
+    translator["-."] = 0x04;
+    translator["--"] = 0x05;
+    translator["..."] = 0x06;
+    translator["..-"] = 0x07;
+    translator[".-."] = 0x08;
+    translator[".--"] = 0x09;
+    translator["-.."] = 0x0A;
+    translator["-.-"] = 0x0B;
+    translator["--."] = 0x0C;
+    translator["---"] = 0x0D;
+    translator["...."] = 0x0E;
+    translator["...-"] = 0x0F;
+    translator[""] = 0x00;
+
+    working = false;
+    was_word_seen = false;
+};
+
+void Downlink::Decoder::clean_buffer() {
+    buffer = "";
+    ret_word = 0x00;
+}
+
+bool Downlink::Decoder::isWorking() {
+    return working;
+}
+
+void Downlink::Decoder::char_cntReset(){
+    was_word_seen = false;
+}
+
+bool Downlink::Decoder::char_cntGet() {
+    return was_word_seen;
+}
+
+void Downlink::Decoder::trySync(int value) {
+    if(sync_state == true) return;
+    if(procState == gather_sync_flg_bits) {
+        if(value == 0) count++;
+        else{
+            // synced and now find expected length
+            expected_len = (int32_t)ceil( (double)count / (sync_flg_bitlen*sync_flg_pop_times));
+            count = 1;
+            flip = 1;
+            sync_state = true;
+            procState = init;
+            cout << "> ";
+            cout.flush();
+            ret_word = 0x00;
+            buffer = "";
+        }
+        return;
+    }
+    if(value == 0) count++;
+    else count = 0;
+    if(count > sync_flg_len) {
+        procState = gather_sync_flg_bits;
+    }
+}
+
+int Downlink::Decoder::process(int value) {
+    trySync(value);
+    if(sync_state == false) return 1;
+    if((value == 1 and flip == 1) or (value == 0 and flip == 0)){
+        count ++;
+        return 1;
+    }
+    if(flip == 1) {
+        // bits dash
+        if(count >= 0.3 * expected_len and count <= 1.5 * expected_len) {
+            buffer.push_back('.');
+        }
+        else if(count > 1.5 * expected_len and count < 2.5 * expected_len) {
+            buffer.push_back('-');
+        }
+        else {
+            // cerr << ":desynced:";
+            if(!(count > 2.8 * expected_len)) {
+                cerr << endl << "::transmission failed::" << endl;
+            }else{
+                cout << ret_word;
+            }
+            sync_state = false;
+            count = 0;
+            flip = 0;
+            procState = init;
+            ret_word = 0x00;
+            return 0;
+        }
+    }
+    else if(flip == 0) {
+        // same word or seperate word
+        if(count >= 0.3 * expected_len and count < 1.5 * expected_len) {
+            // same word
+        }
+        if(count >= 1.5 * expected_len and count <= 2.5 * expected_len) {
+            if(flip_flop == 2) {
+                cout << ret_word;
+                char_cnt = char_cnt | 1;
+                cout.flush();
+                flip_flop = 1;
+                ret_word = 0x00;
+                ret_word = translator[buffer];
+            }
+            else{
+                flip_flop++;
+                ret_word = ret_word << 4;
+                ret_word = ret_word | translator[buffer];
+                was_word_seen = true;
+            }
+            buffer = "";
+        }
+    }
+    flip = value;
+    count = 1;
+    return 1;
+}
+
+Downlink::Receiver::Receiver(float q1, float q0, float sr){
+    q_1=q1;
+    q_0=q0,
+    sample_rate=sr;
+    /*default variables setup*/
+    float a = (2 * M_PI * q_1)/sample_rate;
+    float b = (2 * M_PI * q_0)/sample_rate;
+    float curr_q1 = 0.0f;
+    float curr_q2 = 0.0f;
+    dft_factor_high = vector<complex<float>>(dft_window, complex<float>{0.0f, 0.0f});
+    dft_factor_low = vector<complex<float>>(dft_window, complex<float>{0.0f, 0.0f});
+    int dft_window_factor_remainder = (int32_t)sample_rate % dft_repeat_factor;
+    assert(dft_window_factor_remainder == 0);
+    int size = (int)sample_rate/dft_repeat_factor;
+    reading_high = vector<float>(size, 0.0f);
+    reading_low = vector<float>(size, 0.0f);
+
+    /*precomputing dft multiplication factors*/
+    for(int i =0; i<dft_window; i++){
+        dft_factor_high[i] = exp(complex<float>{0.0f, curr_q1});
+        dft_factor_low[i] = exp(complex<float>{0.0f, curr_q2});
+        curr_q1 += a;
+        curr_q2 += b;
+    }
+
+    /*configuring alsa sound system*/
+    pcm = snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_CAPTURE, 0);
+    assert(pcm >=0);
+    snd_pcm_hw_params_alloca(&params);
+
+    pcm = snd_pcm_hw_params_any(pcm_handle, params);
+    assert(pcm >=0);
+
+    pcm = snd_pcm_hw_params_set_access(pcm_handle, params, SND_PCM_ACCESS_RW_INTERLEAVED);
+    assert(pcm >=0);
+
+    pcm = snd_pcm_hw_params_set_format(pcm_handle, params, SND_PCM_FORMAT_S32);
+    assert(pcm >=0);
+
+    pcm = snd_pcm_hw_params_set_channels(pcm_handle, params, 1);
+    assert(pcm >=0);
+
+    unsigned int _sr = sample_rate;
+    pcm = snd_pcm_hw_params_set_rate_near(pcm_handle, params, &_sr, &dir);
+    assert(pcm >=0);
+
+    frames = 1920;
+    pcm = snd_pcm_hw_params_set_period_size_near(pcm_handle, params, &frames, &dir);
+    assert(pcm >=0);
+
+    pcm = snd_pcm_hw_params(pcm_handle, params);
+    assert(pcm >=0);
+
+    snd_pcm_hw_params_get_period_size(params, &frames, &dir);
+
+    /*buffer setup*/
+    buffer_size = 3 * frames;
+    buffer = (int32_t*) malloc(sizeof(int32_t)*buffer_size);
+}
+
+void Downlink::Receiver::listen() {
+    // using this method only to encounter when dft_window, frames, sampling rate are
+    // not realted via factors and are not dividing in respective manner.
+    memcpy(buffer, buffer + 2 * frames, sizeof(int32_t) * frames);
+    snd_pcm_readi(pcm_handle, buffer+frames, 2 * frames);
+}
+
+void Downlink::Receiver::start() {
+    complex<float> x_q1(0.0f, 0.0f);
+    complex<float> x_q0(0.0f, 0.0f);
+    bool working = true;
+    cout << ">>";
+    cout.flush();
+    decoder.char_cntReset();
+    snd_pcm_prepare(pcm_handle);
+    do{
+        //if(fread(buffer + dft_window, sizeof(int32_t) * sample_rate, 1, stdin) != 1) break;
+        listen();
+        for(int k=0, r=0; k < 2 * frames and working;k+=dft_repeat_factor, r++) {
+            x_q1 = {0, 0};
+            x_q0 = {0, 0};
+            for(int m=0; m < dft_window; m++) {
+                x_q1 += (float)buffer[k+m] * dft_factor_high[m];
+                x_q0 += (float)buffer[k+m] * dft_factor_low[m];
+            }
+            int value = (abs(x_q1) >= abs(x_q0) ? 1 : 0);
+            if(decoder.process(value));
+            else working = false;
+        }
+    }while(working);
+}
+
+
+bool Downlink::Receiver::isBreak() {
+    return !decoder.char_cntGet();
+}
+
+Downlink::Receiver::~Receiver() {
+    snd_pcm_drain(pcm_handle);
+    snd_pcm_close(pcm_handle);
+    free(buffer);
+}
